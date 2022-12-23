acb062bebb97360358535bcffe4aaea3fd160f06
Kyagara <kevinsouzarocha@outlook.com>
2022-12-23 18:47:13 -0300

    refactoring client play connection event

--- src/main/java/net/kyagara/fred/Client.java
+++ src/main/java/net/kyagara/fred/Client.java
@@ -6,11 +6,9 @@ import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.kyagara.fred.command.ModCommands;
 import net.kyagara.fred.config.FredConfig;
+import net.kyagara.fred.event.JoiningWorldEvent;
+import net.kyagara.fred.event.LeavingWorldEvent;
 import net.kyagara.fred.keybind.ModKeybinds;
-import net.minecraft.text.MutableText;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
-import net.minecraft.util.WorldSavePath;
 
 @Environment(EnvType.CLIENT)
 public class Client implements ClientModInitializer {
@@ -20,62 +18,18 @@ public class Client implements ClientModInitializer {
         ModCommands.registerClientCommands();
 
         // Events
-        playConnectionEvents();
+        connectionEvents();
     }
 
-    private void playConnectionEvents() {
+    private static void connectionEvents() {
         ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
             if (!FredConfig.clearChat) {
-                client.inGameHud.getChatHud().addMessage(Text.empty());
-
-                client.inGameHud.getChatHud()
-                        .addMessage(Text.literal("------------------------------------")
-                                .styled(style -> style.withColor(Formatting.AQUA)));
-
-                client.inGameHud.getChatHud().addMessage(Text.empty());
+                LeavingWorldEvent.ClearChat(client);
             }
 
             if (FredConfig.joinMessage) {
-                String worldName;
-
-                boolean isServer = client.world.getServer() != null;
-
-                if (isServer) {
-                    worldName = client.getCurrentServerEntry().name;
-                } else {
-                    worldName = client.getServer().getSavePath(WorldSavePath.ROOT).getParent().getFileName().toString();
-                }
-
-                String day = Long.toString(client.world.getTime() / 24000);
-                String difficulty = client.world.getDifficulty().getName();
-                difficulty = difficulty.substring(0, 1).toUpperCase() + difficulty.substring(1);
-
-                client.inGameHud.getChatHud()
-                        .addMessage(getRow(worldName, "Day: ", day, "Difficulty: ", difficulty));
-
-                if (isServer) {
-                    String count = Integer.toString(client.getCurrentServerEntry().playerListSummary.size());
-                    String ping = Long.toString(client.getCurrentServerEntry().ping);
-
-                    client.inGameHud.getChatHud()
-                            .addMessage(getField("Players: ", count).append("  ").append(getField("Ping: ", ping)));
-                }
-
-                client.inGameHud.getChatHud().addMessage(Text.empty());
+                JoiningWorldEvent.SendJoinMessage(client);
             }
         });
-    }
-
-    private Text getRow(String value1, String name2, String value2, String name3, String value3) {
-        MutableText col1 = Text.literal(value1).styled(style -> style.withBold(true));
-        MutableText col2 = getField(name2, value2);
-        MutableText col3 = getField(name3, value3);
-
-        return col1.append("  ").append(col2).append("  ").append(col3);
-    }
-
-    private MutableText getField(String name, String value) {
-        return Text.literal(name).styled(style -> style.withBold(true))
-                .append(Text.literal(value).styled(style -> style.withBold(false)));
     }
 }
---
+++ src/main/java/net/kyagara/fred/event/JoiningWorldEvent.java
@@ -0,0 +1,50 @@
+package net.kyagara.fred.event;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import net.minecraft.util.WorldSavePath;
+
+public class JoiningWorldEvent {
+    public static void SendJoinMessage(MinecraftClient client) {
+        String worldName;
+
+        boolean isServer = client.world.getServer() != null;
+
+        if (isServer) {
+            worldName = client.getCurrentServerEntry().name;
+        } else {
+            worldName = client.getServer().getSavePath(WorldSavePath.ROOT).getParent().getFileName().toString();
+        }
+
+        String day = Long.toString(client.world.getTime() / 24000);
+        String difficulty = client.world.getDifficulty().getName();
+        difficulty = difficulty.substring(0, 1).toUpperCase() + difficulty.substring(1);
+
+        client.inGameHud.getChatHud()
+                .addMessage(getRow(worldName, "Day: ", day, "Difficulty: ", difficulty));
+
+        if (isServer) {
+            String count = Integer.toString(client.getCurrentServerEntry().playerListSummary.size());
+            String ping = Long.toString(client.getCurrentServerEntry().ping);
+
+            client.inGameHud.getChatHud()
+                    .addMessage(getField("Players: ", count).append("  ").append(getField("Ping: ", ping)));
+        }
+
+        client.inGameHud.getChatHud().addMessage(Text.empty());
+    }
+
+    private static Text getRow(String value1, String name2, String value2, String name3, String value3) {
+        MutableText col1 = Text.literal(value1).styled(style -> style.withBold(true));
+        MutableText col2 = getField(name2, value2);
+        MutableText col3 = getField(name3, value3);
+
+        return col1.append("  ").append(col2).append("  ").append(col3);
+    }
+
+    private static MutableText getField(String name, String value) {
+        return Text.literal(name).styled(style -> style.withBold(true))
+                .append(Text.literal(value).styled(style -> style.withBold(false)));
+    }
+}
