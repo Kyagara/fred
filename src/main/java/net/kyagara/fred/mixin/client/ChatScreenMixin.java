ef7e4ff66427eeac0c863abedf9a1f0de6766a02
Kyagara <kevinsouzarocha@outlook.com>
2023-07-24 17:23:32 -0300

    removing unused imports

--- src/main/java/net/kyagara/fred/mixin/client/ChatScreenMixin.java
+++ src/main/java/net/kyagara/fred/mixin/client/ChatScreenMixin.java
@@ -4,16 +4,11 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.kyagara.fred.Fred;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.hud.MessageIndicator;
 import net.minecraft.client.gui.screen.ChatScreen;
-import net.minecraft.network.message.MessageSignatureData;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.text.Text;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -22,14 +17,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Environment(EnvType.CLIENT)
 @Mixin(ChatScreen.class)
 public abstract class ChatScreenMixin {
-	@Unique
-	@Final
-	private MinecraftClient client = MinecraftClient.getInstance();
+    @Unique
+    @Final
+    private MinecraftClient client = MinecraftClient.getInstance();
 
-	@Inject(method = "onChatFieldUpdate", at = @At("HEAD"))
-	private void onChatFieldUpdate(String chatText, CallbackInfo ci) {
-		if (Fred.CONFIG.enableChatTypingSound() && client != null && client.player != null) {
-			client.player.playSound(SoundEvents.BLOCK_STONE_PLACE, SoundCategory.PLAYERS, Fred.CONFIG.chatTypingVolume(), Fred.CONFIG.chatTypingPitch());
-		}
-	}
+    @Inject(method = "onChatFieldUpdate", at = @At("HEAD"))
+    private void onChatFieldUpdate(String chatText, CallbackInfo ci) {
+        if (Fred.CONFIG.enableChatTypingSound() && client != null && client.player != null) {
+            client.player.playSound(SoundEvents.BLOCK_STONE_PLACE, SoundCategory.PLAYERS, Fred.CONFIG.chatTypingVolume(), Fred.CONFIG.chatTypingPitch());
+        }
+    }
 }
